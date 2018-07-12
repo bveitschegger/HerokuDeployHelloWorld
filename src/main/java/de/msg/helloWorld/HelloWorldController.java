b120ebaffee9627fb5c9f55f7b452ed5c2f10bcc
Bernhard Veitschegger <bernhard.veitschegger@msg.group>
2018-07-12 14:54:10 +0200

    Initial commit

---
+++ src/main/java/de/msg/helloWorld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package de.msg.helloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+
+}
